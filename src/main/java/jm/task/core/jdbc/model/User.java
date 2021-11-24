2a8394ba324450501183b41d1a59646697865ac6
xGeorg23 <89738550+xGeorg23@users.noreply.github.com>
2021-11-24 15:09:03 +0300

    Update User.java

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -59,4 +59,8 @@ public class User {
     public void setAge(Byte age) {
         this.age = age;
     }
+
+    public String toString () {
+        return id + " " + name + " " + lastName + " " + age;
+    }
 }
