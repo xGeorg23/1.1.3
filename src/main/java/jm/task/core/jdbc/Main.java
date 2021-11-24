31b26c20e15b3d3624d177a758e41cea2a14c4de
xGeorg23 <89738550+xGeorg23@users.noreply.github.com>
2021-11-24 15:06:49 +0300

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,20 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
 public class Main {
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Коля","простоКоля",(byte) 20);
+        userService.saveUser("Антон","Идиотикус",(byte) 21);
+        userService.saveUser("Леша","Мудаликус",(byte) 22);
+        userService.saveUser("Гоша","Кидаликус",(byte) 23);
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
