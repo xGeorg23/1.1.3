eb8af1c19e4096f6920cbbb1577b4b3df5ec3af6
xGeorg23 <89738550+xGeorg23@users.noreply.github.com>
2021-11-24 15:11:28 +0300

    Update Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private static final String url = "jdbc:mysql://localhost:3306/mytest";
+    private static final String username = "root";
+    private static final String password = "root1";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+             connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
